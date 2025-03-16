4edb5a519ae67a5cda184f33964bbcefc4f76f97
Andy Christophe <andy.christ971@gmail.com>
2025-03-16 23:14:19 +0100

    fix 404 3t

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,6 +1,12 @@
 const mainController = {
     home: function(req,res){
         res.render('home');
+     },
+
+     notfound: function(req,res){
+        res.status(404).render('error', {
+            message: `La page demandée n'a pas pu être trouvée`,
+          });
      }
 }
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { urlencoded } from 'express';
 import * as dotenv from 'dotenv';
 import router from './app/router.js';
 dotenv.config();
@@ -6,12 +7,16 @@ dotenv.config();
 const port = process.env.PORT;
 const app = express();
 
-
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+
 app.use(router)
-app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('./public'));
+app.use(mainController.notFound);
+
 
 app.listen(port, () => {
     console.log(`serveur sur http://localhost:${port}/`);
