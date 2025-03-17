a1ce9a948e98da8b99deee8a7b6c1d17aa791700
Andy Christophe <andy.christ971@gmail.com>
2025-03-17 17:51:55 +0100

    try fixed err 500 2

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ import { urlencoded } from 'express';
 import * as dotenv from 'dotenv';
 import router from './app/router.js';
 import mainController from './app/controllers/mainController.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const port = process.env.PORT;
 const app = express();
 
 // on configure le chemin vers vers le dossier des vues
-app.set('views', './app/views/');
+app.set('views', __dirname + '/app/views/');
 // on configure le moteur de template
 app.set('view engine', 'ejs');
 
